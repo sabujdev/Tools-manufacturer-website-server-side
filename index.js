3fd75e5dd6b6f353d3c2ac21008f45a6431986c4
sabujmahonto <sabujmahonto@gmail.com>
2022-06-03 18:31:45 +0600

    cors problem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.use(cors({origin:"https://scintillating-banoffee-d13d5b.netlify.app/"}));
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d54lo.mongodb.net/?retryWrites=true&w=majority`;
